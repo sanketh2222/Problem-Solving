e228f8adb61868664923e83b002b11634330e253
unknown <sankeths94@gmail.com>
2021-05-05 01:47:35 +0530

    synchronized method working now

--- multi/src/main/java/com/threads/multi/service/ThreadService2.java
+++ multi/src/main/java/com/threads/multi/service/ThreadService2.java
@@ -10,42 +10,85 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ThreadService2 implements Runnable {
-    static int counter = 1; 
+    static int counter = 1;
 
-    int counter2 = 1; 
+    private volatile boolean isOdd;
+
+    static int counter2 = 1;
 
     static ReentrantLock counterLock = new ReentrantLock(true); // gurantes sync between the 2 threads
 
     public static Logger logger = LoggerFactory.getLogger(ThreadService.class);
 
-    static void incrementCounter(){
+    static void incrementCounter() {
         counterLock.lock();
 
         // Always good practice to enclose locks in a try-finally block
-        try{
-            logger.info("CURRENT THREAD :"+Thread.currentThread().getName() + ": " + counter);
+        try {
+            logger.info("CURRENT THREAD :" + Thread.currentThread().getName() + ": " + counter);
             counter++;
-        }finally{
-             counterLock.unlock();
+        } finally {
+            counterLock.unlock();
         }
     }
 
-    public synchronized void incrementCounter1(){
-      
+    synchronized void incrementCounter1() {
+
         // /Not in-sync as of now
-        while( counter2 <=20){
-            System.out.println(Thread.currentThread().getName() + ": " + counter2);
-            counter2++;
+        while (counter2 <= 20) {
+            try {
+                System.out.println(Thread.currentThread().getName() + ": " + counter2);
+                counter2++;
+                isOdd = true;
+                if (isOdd){
+                    notify();
+                }
+                wait();
+               
+
+            } catch (Exception e) {
+                System.out.println(e.getStackTrace());
+            }
+
+            notify();
 
         }
-            
+
+        // }
+    }
+
+    synchronized void incrementCounter2() {
+
+        // /Not in-sync as of now
+        while (counter2 <= 20) {
+            try {
+
+                System.out.println(Thread.currentThread().getName() + ": " + counter2);
+                counter2++;
+                isOdd = false;
+                if (isOdd){
+                    wait();
+                    isOdd=true;
+                }
+                
+                
+
+            } catch (Exception e) {
+                System.out.println(e.getStackTrace());
+            }
+
+            notify();
+
+        }
+
         // }
     }
 
     @Override
     public void run() {
-        while(counter<=20){
+        while (counter <= 20) {
             incrementCounter1();
+            incrementCounter2();
             // incrementCounter(); //not working as expected need to check this
         }
     }
@@ -56,7 +99,8 @@ public class ThreadService2 implements Runnable {
         Thread thread2 = new Thread(te);
 
         thread1.start();
-        thread2.start();          
+        // thread1.wait();
+        thread2.start();
+
     }
 }
-
