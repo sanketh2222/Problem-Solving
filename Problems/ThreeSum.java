5b5d32871549fa91d465545296f443c60725b5a3
unknown <sankeths94@gmail.com>
2021-05-05 16:40:57 +0530

    enchancement to perform better

--- Problems/ThreeSum.java
+++ Problems/ThreeSum.java
@@ -1,68 +1,69 @@
+import java.util.Scanner;
 import java.util.stream.Collector;
-// import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 import java.util.*;
 
-class ThreeSum {
-
-    public void createTempset() {
-
-    }
-
+public class ThreeSum {
+    
     public List<List<Integer>> threeSum(Integer[] nums) {
-        List<List<Integer>> result1 = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
+       Arrays.sort(nums);
 
-        Set<String> tempset = new HashSet<>();
+        List<List<Integer>> pair = new ArrayList<>();
+        HashSet<String> set = new HashSet<String>();
+        List<Integer> triplets = new ArrayList<>();
 
-        for (int i = 0; i <=nums.length - 1; i++) {
-            for (int j = i + 1; j <= nums.length - 1; j++) {
-                for (int k = j + 1; k <= nums.length - 1; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0//3!
-                            && !tempset.contains(nums[i] + " " + nums[j] + " " + nums[k] )//0 0 0//
-                            && !tempset.contains(nums[i] + " " + nums[k] + " " + nums[j])
-                            && !tempset.contains(nums[j] + " " + nums[k] + " " + nums[i])
-                            && !tempset.contains(nums[j] + " " + nums[i] + " " + nums[k])
-                            && !tempset.contains(nums[k] + " " + nums[i] + " " + nums[j])
-                            && !tempset.contains(nums[k]+ " " + nums[j] + " " + nums[i])//
-                    ) {
-                        // System.out.printf("rsult is"+nums[i]+""+nums[j]+""+nums[k]);
+        /*
+         * Iterate over the array from the start and consider it as the first element
+         */
+        for (int i = 0; i < nums.length - 2; i++) {
 
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
+            // index of the first element in the
+            // remaining elements
+            int j = i + 1;
 
-                        // if ()
+            // index of the last element
+            int k = nums.length - 1;
 
-                        tempset.add(nums[i] + " " + nums[j] + " " + nums[k]);
+            while (j < k) {
 
+                if (nums[i] + nums[j] + nums[k] == 0) {
 
-                        // temp1 = temp.stream().collect(Collectors.toList());//O(1)   {}()
-                        // temp1 = new ArrayList<>();
-                        // temp1.addAll(temp);
+                    String str = nums[i] + ":" + nums[j] + ":" + nums[k];
 
-                        // result1.
-                        // temp1=temp;
-                        // // System.out.printf("temp is"+temp);
-                        result1.add(temp);// temp
-                        // // System.out.printf("add res is"+result);
-                        // temp.clear();
-                        temp = new ArrayList<>();
+                    if (!set.contains(str)) {
+
+                        // To check for the unique triplet
+                        triplets.add(nums[i]);
+                        triplets.add(nums[j]);
+                        triplets.add(nums[k]);
+                        pair.add(triplets);
+                        triplets = new ArrayList<>();
+                        set.add(str);
                     }
-                }
 
+                    // increment the second value index
+                    j++;
+
+                    // decrement the third value index
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0)
+                    j++;
+
+                else
+                    k--;
             }
-            // System.out.printf("add res is"+result1);
         }
-        // for (int i=0;i<temp.size();i+=3){
-
-        // }
-        // System.out.printf("rsult is"+result1);
-        return result1;
+        return pair;
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // 6
+        // -1 0 1 2 -1 -4
 
+        // -1 0 1
+        // -1 -1 2
         Integer n = scanner.nextInt();
         Integer nums[] = new Integer[n];
         for(Integer i = 0 ; i < n ;i++) {
